a9d45f00962bc132dab06b42dfc5883c9babae21
liyefei737 <macrofly@gmail.com>
2020-01-17 10:23:32 -0800

    log out a mssg when a line in input is invalid

--- main.py
+++ main.py
@@ -10,20 +10,18 @@ def simimgs(in_csv_path, out_csv_path):
             with open(out_csv_path, 'wt', encoding='utf-8') as csv_out:
                 csv_reader = csv.reader(csv_in, delimiter=',')
                 csv_writer = csv.writer(csv_out, delimiter=',')
-                next(csv_reader)
+                next(csv_reader) # skip input csv header
                 csv_writer.writerow(['image1', 'image2', 'similar', 'elapsed'])
 
-                for row in csv_reader:
-                    # csv_writer.writerow(row)
-                    # print(row)
-                    im1_path, im2_path = row
-                    # 1 or 2 image files do not exist in the computer
-                    # log out which paths do not exist and move on the the next paris
+                for line_numb, row in enumerate(csv_reader, start=1):
+                    im1_path, im2_path = row[0], row[1]
                     if not os.path.exists(im1_path) or not os.path.exists(im2_path):
-                        # continue
-                        pass
-
-                    # both file exist
+                        print("Invalid line {} in input csv".format(line_numb))
+                        if not os.path.exists(im1_path):
+                            print("--- Image 1 has an invalid path: {}".format(im1_path))
+                        if not os.path.exists(im2_path):
+                            print("--- Image 2 has an invalid path: {}".format(im2_path))
+                        continue
                     else:
                         similarity_score, run_time = timed_call(get_similarity, [im1_path, im2_path])
                         csv_writer.writerow([im1_path, im2_path, similarity_score, run_time])
